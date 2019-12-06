0ef427771cc82bb2f66e75cb551ccba7e085ca1f
Scott Byrne <scott.byrne@forestry.io>
2019-12-06 14:18:15 -0400

    fix: add sidebar config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,10 @@ module.exports = {
     {
       resolve: "gatsby-plugin-tinacms",
       options: {
+        sidebar: {
+          hidden: process.env.NODE_ENV === "production",
+          position: "displace",
+        },
         plugins: ["gatsby-tinacms-git", "gatsby-tinacms-remark"],
       },
     },
